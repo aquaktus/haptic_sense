14cfdc2946ac509b8dff4efde890d44e941459cc
carlos gemmell <aquaktus@yahoo.co.uk>
2019-11-03 19:43:53 +0000

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,7 @@
 from haptic_driver import hapticDriver
 
-hptc = hapticDriver(4,0.3,True)
+hptc = hapticDriver(4,0.7,True)
 
-message = "hello world"
+message = "hello world from carlos and eric in their living room"
+alphabet = "a b c d e f g  h i j k  lmnop  q r s  t u v  w x  y  z"
 hptc.write_message(message)
